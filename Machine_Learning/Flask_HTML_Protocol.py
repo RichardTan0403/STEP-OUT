a47cccdf732d6810dfeb29ad7ca067a5012c3e58
RichardTan0403 <yenson.tan@gmail.com>
2023-06-03 12:01:12 +0800

    folder updates

--- Machine_Learning/Flask_HTML_Protocol.py
+++ Machine_Learning/Flask_HTML_Protocol.py
@@ -2,39 +2,11 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-app = Flask(__name__, template_folder= 'template')
+app = Flask(__name__, template_folder= './Machine_Learning/templates', static_folder='./Machine_Learning/static')
 
 @app.route('/')
 def greeting():
     return render_template('patient.html')
-
-# @app.route('/data/', methods = ['POST', 'GET'])
-# def data():
-#     form_data = request.form
-#     if request.method == 'GET':
-#         return str(list(form_data.values()))
-#     if request.method == 'POST':
-#         form_data = request.form
-#         return render_template('data.html',form_data = form_data)
-
-# @app.route('/model')
-# def model():
-#     loaded_model = pickle.load(open('Capstone_RFC_model.sav','rb'))
-#     a = [1,1,0,0,0,1,1,1,0,1,0]
-#     a = np.expand_dims(a,0)
-#     result = str(loaded_model.predict(a))
-#     return result
-
-
-# @app.route("/calculate-estimate", methods=["GET"])
-# def calculate_estimate():
-#     estimate = request.args.get("estimate")
-#     return estimate
-
-# @app.route("/fav_language", methods=["GET"])
-# def fav_language():
-#     estimate = request.args.get("fav_language")
-#     return estimate
 
 @app.route("/result", methods=['POST'])
 def predict():
